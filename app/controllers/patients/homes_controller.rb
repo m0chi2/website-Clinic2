e9d66710ac06c15a02d4d21d64f16c9266daced2
m0chi2 <20saki19@gmail.com>
2020-07-24 15:01:20 +0000

    [DELETE]unnecessary files

--- app/controllers/patients/homes_controller.rb
+++ app/controllers/patients/homes_controller.rb
@@ -3,9 +3,6 @@ class Patients::HomesController < ApplicationController
 	def index
 	end
 
-	def show
-	end
-
 	def reservation
 		@reservations = Reservation.where(membership_number: current_patient.membership_number)
 		@reservations = @reservations.page(params[:page]).reverse_order
