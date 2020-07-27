804a6d7e366e00fe1f0c4df4f77cef886a855ff4
m0chi2 <20saki19@gmail.com>
2020-07-27 10:12:57 +0000

    [Fix]layout

--- app/assets/javascripts/pickadate.js
+++ app/assets/javascripts/pickadate.js
@@ -1,9 +1,14 @@
   $(function(){
     $("#datepicker").pickadate({
-    format: "yyyy年mm月dd日"
+    format: "yyyy年mm月dd日",
+       min: +1,
+       max: +90
     });
 
     $("#timepicker").pickatime({
     format: "HH:i"
+    interval: 30,
+    min: [8,30],
+    max: [18,00]
     });
     });
