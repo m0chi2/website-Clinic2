6f0d65ebc05b2ed4b563af3e043bce5cea9c02ce
m0chi2 <20saki19@gmail.com>
2020-07-27 23:38:49 +0000

    [Fix]mailer for deploy

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ require 'action_mailer'
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
+  address: "smtp.gmail.com",
   domain: 'gmail.com',
   port: 587,
   user_name: ENV["EMAIL_ADDRESS"],
